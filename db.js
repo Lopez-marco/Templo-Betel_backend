c7d585af5a8aac49d605f6774b7c7d8a5ecfa127
Marco Lopez <lopez.marcohdez@gmail.com>
2020-12-31 09:37:48 -0500

    push 3

--- db.js
+++ db.js
@@ -1,12 +1,11 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('templo-betel', 'postgres', 'password', {
-    host: 'localhost',
-    dialect: 'postgres'
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
 });
 
 sequelize.authenticate().then(
     function () {
-        console.log('Connected to worklog postgres database');
+        console.log('Connected');
     },
     function (err) {
         console.log(err);
